b914fdfc966d046a6f2fb742b40e42c8a4f3b695
sajjadhossain <sajjadhossain0756@gmail.com>
2024-09-19 08:42:11 +0600

    login form done

---
+++ JS/home.js
@@ -0,0 +1,22 @@
+
+// document.getElementById('add-money-button').addEventListener('click',function(event){
+
+//     event.preventDefault();
+//     const addAmount = document.getElementById('add-amount').value;
+//     const pinNumber = document.getElementById('pin-number').value;
+//     if(pinNumber === '1234'){
+//         let balance = document.getElementById('total-balance').innerText;
+    
+//         let addMoney = Number(addAmount);
+//         let totalBalance = Number(balance);
+
+//         let newBalance = totalBalance + addMoney;
+        
+//         document.getElementById('total-balance').innerText = newBalance;
+        
+    
+//     }
+//     else{
+//         alert('you money is not added');
+//     }
+// })
---
+++ JS/login.js
@@ -0,0 +1,14 @@
+
+document.getElementById('login-button').addEventListener('click',function(event){
+      
+      event.preventDefault();
+      const phoneNumber = document.getElementById('phone-number').value;
+      const pinNumber = document.getElementById('pin-number').value;
+      
+      if(pinNumber === '1234'){
+          window.location.href = '/home.html';
+      }
+      else{
+        alert('your mobile number or pin number is incorrect !');
+      }
+})
