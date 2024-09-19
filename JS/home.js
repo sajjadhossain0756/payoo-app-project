4e976c9ccbcb98cd7ee42e6cda28f31eed477825
sajjadhossain <sajjadhossain0756@gmail.com>
2024-09-19 08:47:56 +0600

    home page done

--- JS/home.js
+++ JS/home.js
@@ -1,22 +1,22 @@
 
-// document.getElementById('add-money-button').addEventListener('click',function(event){
+document.getElementById('add-money-button').addEventListener('click',function(event){
 
-//     event.preventDefault();
-//     const addAmount = document.getElementById('add-amount').value;
-//     const pinNumber = document.getElementById('pin-number').value;
-//     if(pinNumber === '1234'){
-//         let balance = document.getElementById('total-balance').innerText;
+    event.preventDefault();
+    const addAmount = document.getElementById('add-amount').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    if(pinNumber === '1234'){
+        let balance = document.getElementById('total-balance').innerText;
     
-//         let addMoney = Number(addAmount);
-//         let totalBalance = Number(balance);
+        let addMoney = Number(addAmount);
+        let totalBalance = Number(balance);
 
-//         let newBalance = totalBalance + addMoney;
+        let newBalance = totalBalance + addMoney;
         
-//         document.getElementById('total-balance').innerText = newBalance;
+        document.getElementById('total-balance').innerText = newBalance;
         
     
-//     }
-//     else{
-//         alert('you money is not added');
-//     }
-// })
+    }
+    else{
+        alert('you money is not added');
+    }
+})
